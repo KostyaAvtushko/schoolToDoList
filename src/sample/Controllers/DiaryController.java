d246f226322c6daa5b3b48e8ccf81e534da70f87
avtushko2004 <62934228+avtushko2004@users.noreply.github.com>
2021-05-08 23:30:45 +0300

    Update DiaryController.java

--- src/sample/Controllers/DiaryController.java
+++ src/sample/Controllers/DiaryController.java
@@ -14,6 +14,7 @@ import org.sqlite.SQLiteDataSource;
 import java.awt.*;
 import java.io.*;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -62,6 +63,29 @@ public class DiaryController {
     private Text employmentSunday;
 
     @FXML
+    private Text dateMonday;
+
+    @FXML
+    private Text dateTuesday;
+
+    @FXML
+    private Text dateWednesday;
+
+    @FXML
+    private Text dateThursday;
+
+
+    @FXML
+    private Text dateFriday;
+
+    @FXML
+    private Text dateSaturday;
+
+    @FXML
+    private Text dateSunday;
+
+
+    @FXML
     private Button sundayBtn;
 
     @FXML
@@ -108,6 +132,13 @@ public class DiaryController {
                     String friday = resultSet.getString(5);
                     String saturday = resultSet.getString(6);
                     String sunday = resultSet.getString(7);
+                    String dateMondaySQL = resultSet.getString(9);
+                    String dateTuesdaySQL = resultSet.getString(10);
+                    String dateWednesdaySQL = resultSet.getString(11);
+                    String dateThursdaySQL = resultSet.getString(12);
+                    String dateFridaySQL = resultSet.getString(13);
+                    String dateSaturdaySQL = resultSet.getString(14);
+                    String dateSundaySQL = resultSet.getString(15);
 
                     employmentMonday.setText(monday);
                     employmentTuesday.setText(tuesday);
@@ -116,6 +147,13 @@ public class DiaryController {
                     employmentFriday.setText(friday);
                     employmentSaturday.setText(saturday);
                     employmentSunday.setText(sunday);
+                    dateMonday.setText(dateMondaySQL);
+                    dateTuesday.setText(dateTuesdaySQL);
+                    dateWednesday.setText(dateWednesdaySQL);
+                    dateThursday.setText(dateThursdaySQL);
+                    dateFriday.setText(dateFridaySQL);
+                    dateSaturday.setText(dateSaturdaySQL);
+                    dateSunday.setText(dateSundaySQL);
                 }
                 con.commit();
                 con.endRequest();
@@ -124,19 +162,21 @@ public class DiaryController {
             e.printStackTrace();
         }
 
-
         mondayBtn.setOnAction(actionEvent -> {
+            LocalDate localDate = LocalDate.now();
             TextInputDialog dialog = new TextInputDialog("Ваше занятие");
             dialog.setTitle("Ваше занятие");
             dialog.setHeaderText("Ваше занятие");
             dialog.setContentText("Введите ваше занятие:");
             Optional<String> result = dialog.showAndWait();
             employmentMonday.setText(result.get());
+            dateMonday.setText(localDate.toString());
             try(Connection connectionMonday = DriverManager.getConnection(url, dbHandler, dbPass)){
-                String inputMonday = "UPDATE WEEK SET monday=?";
+                String inputMonday = "UPDATE WEEK SET monday=?, datemonday=?";
                 try(PreparedStatement preparedStatementMonday = connectionMonday.prepareStatement(inputMonday)){
                     connectionMonday.setAutoCommit(false);
                     preparedStatementMonday.setString(1, result.get());
+                    preparedStatementMonday.setString(2, localDate.toString());
                     preparedStatementMonday.executeUpdate();
                     connectionMonday.commit();
                 }
@@ -145,17 +185,20 @@ public class DiaryController {
             }
         });
         tuesdayBtn.setOnAction(actionEvent -> {
+            LocalDate localDate = LocalDate.now();
             TextInputDialog dialog = new TextInputDialog("Ваше занятие");
             dialog.setTitle("Ваше занятие");
             dialog.setHeaderText("Ваше занятие");
             dialog.setContentText("Введите ваше занятие:");
+            dateTuesday.setText(localDate.toString());
             Optional<String> result = dialog.showAndWait();
             employmentTuesday.setText(result.get());
             try(Connection connectionTuesday = DriverManager.getConnection(url, dbHandler, dbPass)){
-                String inputTuesday = "UPDATE WEEK SET tuesday=?";
+                String inputTuesday = "UPDATE WEEK SET tuesday=?, datetuesday=?";
                 try(PreparedStatement preparedStatementTuesday = connectionTuesday.prepareStatement(inputTuesday)){
                     connectionTuesday.setAutoCommit(false);
                     preparedStatementTuesday.setString(1, result.get());
+                    preparedStatementTuesday.setString(2, localDate.toString());
                     preparedStatementTuesday.executeUpdate();
                     connectionTuesday.commit();
                 }
@@ -164,17 +207,20 @@ public class DiaryController {
             }
         });
         wednesdayBtn.setOnAction(actionEvent -> {
+            LocalDate localDate = LocalDate.now();
             TextInputDialog dialog = new TextInputDialog("Ваше занятие");
             dialog.setTitle("Ваше занятие");
             dialog.setHeaderText("Ваше занятие");
             dialog.setContentText("Введите ваше занятие:");
             Optional<String> result = dialog.showAndWait();
             employmentWednesday.setText(result.get());
+            dateWednesday.setText(localDate.toString());
             try(Connection connectionWednesday = DriverManager.getConnection(url, dbHandler, dbPass)){
-                String inputWednesday = "UPDATE WEEK SET wednesday=?";
+                String inputWednesday = "UPDATE WEEK SET wednesday=?, datewednesday=?";
                 try(PreparedStatement preparedStatementWednesday = connectionWednesday.prepareStatement(inputWednesday)){
                     connectionWednesday.setAutoCommit(false);
                     preparedStatementWednesday.setString(1, result.get());
+                    preparedStatementWednesday.setString(2, localDate.toString());
                     preparedStatementWednesday.executeUpdate();
                     connectionWednesday.commit();
                 }
@@ -183,17 +229,20 @@ public class DiaryController {
             }
         });
         thursdayBtn.setOnAction(actionEvent -> {
+            LocalDate localDate = LocalDate.now();
             TextInputDialog dialog = new TextInputDialog("Ваше занятие");
             dialog.setTitle("Ваше занятие");
             dialog.setHeaderText("Ваше занятие");
             dialog.setContentText("Введите ваше занятие:");
             Optional<String> result = dialog.showAndWait();
             employmentThursday.setText(result.get());
+            dateThursday.setText(localDate.toString());
             try(Connection connectionThursday = DriverManager.getConnection(url, dbHandler, dbPass)){
-                String inputThursday = "UPDATE WEEK SET thursday=?";
+                String inputThursday = "UPDATE WEEK SET thursday=?, datethursday=?";
                 try(PreparedStatement preparedStatementThursday = connectionThursday.prepareStatement(inputThursday)){
                     connectionThursday.setAutoCommit(false);
                     preparedStatementThursday.setString(1, result.get());
+                    preparedStatementThursday.setString(2, localDate.toString());
                     preparedStatementThursday.executeUpdate();
                     connectionThursday.commit();
                 }
@@ -202,17 +251,20 @@ public class DiaryController {
             }
         });
         fridayBtn.setOnAction(actionEvent -> {
+            LocalDate localDate = LocalDate.now();
             TextInputDialog dialog = new TextInputDialog("Ваше занятие");
             dialog.setTitle("Ваше занятие");
             dialog.setHeaderText("Ваше занятие");
             dialog.setContentText("Введите ваше занятие:");
             Optional<String> result = dialog.showAndWait();
             employmentFriday.setText(result.get());
+            dateFriday.setText(localDate.toString());
             try(Connection connectionFriday = DriverManager.getConnection(url, dbHandler, dbPass)){
-                String inputFriday = "UPDATE WEEK SET friday=?";
+                String inputFriday = "UPDATE WEEK SET friday=?, datefriday=?";
                 try(PreparedStatement preparedStatementFriday = connectionFriday.prepareStatement(inputFriday)){
                     connectionFriday.setAutoCommit(false);
                     preparedStatementFriday.setString(1, result.get());
+                    preparedStatementFriday.setString(2, localDate.toString());
                     preparedStatementFriday.executeUpdate();
                     connectionFriday.commit();
                 }
@@ -221,17 +273,20 @@ public class DiaryController {
             }
         });
         saturdayBtn.setOnAction(actionEvent -> {
+            LocalDate localDate = LocalDate.now();
             TextInputDialog dialog = new TextInputDialog("Ваше занятие");
             dialog.setTitle("Ваше занятие");
             dialog.setHeaderText("Ваше занятие");
             dialog.setContentText("Введите ваше занятие:");
             Optional<String> result = dialog.showAndWait();
             employmentSaturday.setText(result.get());
+            dateSaturday.setText(localDate.toString());
             try(Connection connectionSaturday = DriverManager.getConnection(url, dbHandler, dbPass)){
-                String inputSaturday = "UPDATE WEEK SET saturday=?";
+                String inputSaturday = "UPDATE WEEK SET saturday=?, datesaturday=?";
                 try(PreparedStatement preparedStatementSaturday = connectionSaturday.prepareStatement(inputSaturday)){
                     connectionSaturday.setAutoCommit(false);
                     preparedStatementSaturday.setString(1, result.get());
+                    preparedStatementSaturday.setString(2, localDate.toString());
                     preparedStatementSaturday.executeUpdate();
                     connectionSaturday.commit();
                 }
@@ -240,17 +295,20 @@ public class DiaryController {
             }
         });
         sundayBtn.setOnAction(actionEvent -> {
+            LocalDate localDate = LocalDate.now();
             TextInputDialog dialog = new TextInputDialog("Ваше занятие");
             dialog.setTitle("Ваше занятие");
             dialog.setHeaderText("Ваше занятие");
             dialog.setContentText("Введите ваше занятие:");
             Optional<String> result = dialog.showAndWait();
             employmentSunday.setText(result.get());
+            dateSunday.setText(localDate.toString());
             try(Connection connectionSunday = DriverManager.getConnection(url, dbHandler, dbPass)){
-                String inputSunday = "UPDATE WEEK SET sunday=?";
+                String inputSunday = "UPDATE WEEK SET sunday=?, datesunday=?";
                 try(PreparedStatement preparedStatementSunday = connectionSunday.prepareStatement(inputSunday)){
                     connectionSunday.setAutoCommit(false);
                     preparedStatementSunday.setString(1, result.get());
+                    preparedStatementSunday.setString(2, localDate.toString());
                     preparedStatementSunday.executeUpdate();
                     connectionSunday.commit();
                 }
